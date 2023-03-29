fbe617f012a72ba4f49a2963ae89e966d58b15fa
lucasgomes14 <lucazmatehus14@gmail.com>
2023-03-29 17:30:29 -0300

    initialing database connection

---
+++ TodoApp/app/src/main/java/Util/ConnectionFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Util;
+
+/**
+ *
+ * @author lucas
+ */
+public class ConnectionFactory {
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql>//localhost:3306/TodoApp";
+    public static final String USER = "curso";
+    public static final String PASS = "12345678";
+    }
